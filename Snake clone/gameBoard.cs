d73d17beb3539dcc4446bc97d6779ee86f2f4768
ClayFlores <ClayFlores@users.noreply.github.com>
2020-03-31 16:29:49 -0700

    Big update
    adds movement
    adds increasing snake length

--- Snake clone/gameBoard.cs
+++ Snake clone/gameBoard.cs
@@ -59,17 +59,130 @@ namespace Snake_clone
         }
         internal void move()
         {
-            // if next space == ' ' || next space == 'o' then everything is alright
-
-            // else isAlive = false
-
-            isAlive = false;
+            // this is going to be a huge function. could break it into diff functions for each direction
+            switch (currDirection)
+            {
+                case Direction.right:
+                    // monster of an if statement
+                    // checks if the value 1 space up is a wall or part of the snake
+                    if (Board[Snake.Last().xCoord][Snake.Last().yCoord + 1] == 'X' || Board[Snake.Last().xCoord][Snake.Last().yCoord + 1] == '=')
+                    {
+                        isAlive = false;
+                    }
+                    // not a death but an apple
+                    else if (Board[Snake.Last().xCoord][Snake.Last().yCoord + 1] == 'o')
+                    {
+                        snake temp = new snake();
+                        temp.xCoord = Snake.Last().xCoord;
+                        temp.yCoord = Snake.Last().yCoord + 1;
+                        Snake.Add(temp);
+                        Board[temp.xCoord][temp.yCoord] = '=';
+                        applesEaten++;
+                    }
+                    else
+                    {
+                        snake temp = new snake();
+                        temp.xCoord = Snake.Last().xCoord;
+                        temp.yCoord = Snake.Last().yCoord + 1;
+                        Snake.Add(temp);
+                        Board[temp.xCoord][temp.yCoord] = '=';
+                        // remove oldest snake
+                        Board[Snake[0].xCoord][Snake[0].yCoord] = ' ';
+                        Snake.RemoveAt(0); // the first item in the list should be the oldest snake piece
+                    }
+                    break;
+                case Direction.left:
+                    if (Board[Snake.Last().xCoord][Snake.Last().yCoord - 1] == 'X' || Board[Snake.Last().xCoord][Snake.Last().yCoord - 1] == '=')
+                    {
+                        isAlive = false;
+                    }
+                    // not a death but an apple
+                    else if (Board[Snake.Last().xCoord][Snake.Last().yCoord - 1] == 'o')
+                    {
+                        snake temp = new snake();
+                        temp.xCoord = Snake.Last().xCoord;
+                        temp.yCoord = Snake.Last().yCoord - 1;
+                        Snake.Add(temp);
+                        Board[temp.xCoord][temp.yCoord] = '=';
+                        applesEaten++;
+                    }
+                    else
+                    {
+                        snake temp = new snake();
+                        temp.xCoord = Snake.Last().xCoord;
+                        temp.yCoord = Snake.Last().yCoord - 1;
+                        Snake.Add(temp);
+                        Board[temp.xCoord][temp.yCoord] = '=';
+                        // remove oldest snake
+                        Board[Snake[0].xCoord][Snake[0].yCoord] = ' ';
+                        Snake.RemoveAt(0); // the first item in the list should be the oldest snake piece
+                    }
+                    break;
+                case Direction.up:
+                    if (Board[Snake.Last().xCoord - 1][Snake.Last().yCoord] == 'X' || Board[Snake.Last().xCoord - 1][Snake.Last().yCoord] == '=')
+                    {
+                        isAlive = false;
+                    }
+                    // not a death but an apple
+                    else if (Board[Snake.Last().xCoord - 1][Snake.Last().yCoord] == 'o')
+                    {
+                        snake temp = new snake();
+                        temp.xCoord = Snake.Last().xCoord - 1;
+                        temp.yCoord = Snake.Last().yCoord;
+                        Snake.Add(temp);
+                        Board[temp.xCoord][temp.yCoord] = '=';
+                        applesEaten++;
+                    }
+                    else
+                    {
+                        snake temp = new snake();
+                        temp.xCoord = Snake.Last().xCoord - 1;
+                        temp.yCoord = Snake.Last().yCoord;
+                        Snake.Add(temp);
+                        Board[temp.xCoord][temp.yCoord] = '=';
+                        // remove oldest snake
+                        Board[Snake[0].xCoord][Snake[0].yCoord] = ' ';
+                        Snake.RemoveAt(0); // the first item in the list should be the oldest snake piece
+                    }
+                    break;
+                case Direction.down:
+                    if (Board[Snake.Last().xCoord + 1][Snake.Last().yCoord] == 'X' || Board[Snake.Last().xCoord + 1][Snake.Last().yCoord] == '=')
+                    {
+                        isAlive = false;
+                    }
+                    // not a death but an apple
+                    else if (Board[Snake.Last().xCoord + 1][Snake.Last().yCoord] == 'o')
+                    {
+                        snake temp = new snake();
+                        temp.xCoord = Snake.Last().xCoord + 1;
+                        temp.yCoord = Snake.Last().yCoord;
+                        Snake.Add(temp);
+                        Board[temp.xCoord][temp.yCoord] = '=';
+                        applesEaten++;
+                    }
+                    else
+                    {
+                        snake temp = new snake();
+                        temp.xCoord = Snake.Last().xCoord + 1;
+                        temp.yCoord = Snake.Last().yCoord;
+                        Snake.Add(temp);
+                        Board[temp.xCoord][temp.yCoord] = '=';
+                        // remove oldest snake
+                        Board[Snake[0].xCoord][Snake[0].yCoord] = ' ';
+                        Snake.RemoveAt(0); // the first item in the list should be the oldest snake piece
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Something went wrong here...");
+                    break;
+            } 
         }
         
 
         // prints out the board
         public void printBoard()
         {
+            // TODO: make sure the snake is in the board before printing it
             Console.Clear();
             foreach(var subList in Board)
             {
@@ -84,18 +197,18 @@ namespace Snake_clone
 
         internal void gameOver()
         {
-            Board[5][7] = ' ';
-            Board[5][8] = 'O';
-            Board[5][9] = 'V';
-            Board[5][10] = 'E';
-            Board[5][11] = 'R';
-            Board[5][12] = ' ';
             Board[4][7] = ' ';
             Board[4][8] = 'G';
             Board[4][9] = 'A';
             Board[4][10] = 'M';
             Board[4][11] = 'E';
             Board[4][12] = ' ';
+            Board[5][7] = ' ';
+            Board[5][8] = 'O';
+            Board[5][9] = 'V';
+            Board[5][10] = 'E';
+            Board[5][11] = 'R';
+            Board[5][12] = ' ';
             printBoard();
         }
     }
